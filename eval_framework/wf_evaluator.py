daca8fcbdaa8284353aa8eddafe1319296ca46bf
Dennis <65568528+zannads@users.noreply.github.com>
2023-12-24 12:56:11 +0100

    dashboard functions code reorganisation

--- eval_framework/wf_evaluator.py
+++ eval_framework/wf_evaluator.py
@@ -43,6 +43,7 @@ from constants import DAY_LEN, WEEK_LEN
 import data_loader
 from .performance_indicators import performance_indicators, performance_indicators_long_names, performance_indicators_labels
 from .result_structures import ModelResults, ProcessResults
+from .dashb_funcs import wf_dashboard_layout, figure_perf_ind_trajectory, figure_dma_q_trajectory
 
 class WaterFuturesEvaluator:
 
@@ -293,56 +294,7 @@ class WaterFuturesEvaluator:
         """
         self.app = Dash(__name__)
 
-        self.app.layout = html.Div([
-            html.H1(children='Water Futures Evaluator Dashboard', style={'textAlign': 'center'}),
-            html.Div([
-                html.Div([
-                    html.Div(children='Select the performance indicator to visualize:'),
-                    dcc.Dropdown(['PI1', 'PI2', 'PI3'], 'PI1', id='pi-dropdown'),
-                    html.Div(children={}, id='pi-description')
-                ], style={'width': '30%', 'display': 'inline-block'}),
-                html.Div([
-                    html.Div(children='Select the DMA to visualize:'),
-                    dcc.Dropdown(data_loader.DMAS_NAMES, data_loader.DMAS_NAMES[0], id='dma-dropdown'),
-                    html.Div(children={}, id='dma-description')
-                ], style={'width': '30%', 'float': 'left', 'display': 'inline-block'}),
-                html.Div([
-                        html.Div(children='Messages from the dashboard:'),
-                        html.Div(children={}, id='errors-description'),
-                ], style={'width': '40%', 'float': 'right', 'display': 'inline-block'})
-            ]),
-            dcc.Graph(id='graph-content'),
-            html.Div([ 
-                html.Div([ 
-                    html.Div(children='Select the models to visualize:'),
-                    dcc.Checklist(
-                        id="model-checklist",
-                        options=self.models_names(),
-                        value=[self.models_names()[0]],
-                        inline=True
-                    )
-                ], style={'width': '48%', 'display': 'inline-block'}),
-                html.Div([
-                    html.Div(children='Select the Model Description to read:'),
-                    dcc.Dropdown(self.models_names(), self.models_names()[0], id='model-description-dropdown'),
-                    html.Div(children={}, id='model-description')
-                ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),
-            ]),
-            html.Div([
-                dcc.Graph(id='trajectory-content'),
-                dcc.RangeSlider(
-                    id='trajectory-slider',
-                    min=self.first_split_absweek(),
-                    max=self.fcst_absweek(),
-                    step=1,
-                    value=[self.first_test_absweek(),
-                           self.fcst_absweek()],
-                    marks={i: '{}'.format(i) for i in range(self.first_split_absweek(),
-                                                            self.fcst_absweek()+1,
-                                                            1)}
-                )
-            ])
-        ])
+        self.app.layout = wf_dashboard_layout(self)
 
         @callback(
             Output('graph-content', 'figure'),
@@ -356,87 +308,16 @@ class WaterFuturesEvaluator:
             Input('trajectory-slider', 'value')
         )
         def update_dash(dma, pi, model_names, wrange):
-            figpi = go.Figure()
             
-            i = 0
             # limit the number of models to show to the number of colors that can be used
             message = ""
             if len(model_names) > len(px.colors.qualitative.Plotly):
                 model_names = model_names[:len(px.colors.qualitative.Plotly)]
                 message = "Too many models selected, only the first {} will be shown.\n".format(len(px.colors.qualitative.Plotly))
 
-            for model_name in model_names:
-                vali__df = self.__models_results[model_name]["validation"]
-                vali__weeks = vali__df.index.get_level_values(0).unique().to_numpy()
-                vali__dma_pi = vali__df.xs(dma, level=1).loc[:,pi].to_numpy()
-
-                test__df = self.__models_results[model_name]["test"]
-                test__weeks = test__df.index.get_level_values(0).unique().to_numpy()
-                test__dma_pi = test__df.xs(dma, level=1).loc[:,pi].to_numpy()
-
-                # A line with the PI time series duringthe validation and the average 
-                # of the PI during the test
-                plot__weeks = np.append(vali__weeks, test__weeks[[0,-1]])
-                plot__dma_pi = np.append(vali__dma_pi, [test__dma_pi.mean(), test__dma_pi.mean()])
-
-                # add the line to the plot and the 4 points indepentently
-                figpi.add_trace(go.Scatter(x=plot__weeks, y=plot__dma_pi, name=model_name, 
-                                         mode='lines', line=dict(color=px.colors.qualitative.Plotly[i])))
-                figpi.add_trace(go.Scatter(x=test__weeks, y=test__dma_pi, name=model_name, 
-                                         mode='markers', marker=dict(color=px.colors.qualitative.Plotly[i]), 
-                                         showlegend=False))
-
-                figpi.update_xaxes(range=[vali__weeks[0]-1, test__weeks[-1]+1])
-                week_ticks = week_ticks = np.concatenate(([plot__weeks[0]], 
-                                                          np.arange(20, plot__weeks[-1], 10), 
-                                                          test__weeks.tolist())).tolist()
-                figpi.update_xaxes(tickvals=week_ticks)
-
-                figpi.update_layout(
-                    legend=dict(
-                        x=0.7,
-                        y=1.11,
-                        orientation="h",
-                        traceorder="normal",
-                        font=dict(
-                            family="sans-serif",
-                            size=12,
-                            color="black"
-                        ),
-                        #bgcolor="LightSteelBlue",
-                        #bordercolor="Black",
-                        #borderwidth=2,
-                        #xanchor='center',  # Anchor the legend at its center
-                        #yanchor='bottom',  # Anchor the legend at its bottom
-                    )
-                )
-                i += 1
+            figpi = figure_perf_ind_trajectory(self, dma, pi, model_names)
             
-            figpi.update_layout(title=f"Performance for {dma} and {pi} of the selected models in Validation and Test", 
-                              xaxis_title='Week', yaxis_title=performance_indicators_labels[pi])
-            
-            # start of the plot with trajectories 
-            figtraj = go.Figure()
-
-            # This start from self.__first_split_week
-            start_h = pd.to_datetime(data_loader.monday_of_week_number(wrange[0]))
-            end_h = pd.to_datetime(data_loader.monday_of_week_number(wrange[1]+1))
-            i = 0
-            for model_name in model_names:
-                pred__dma_h_q = self.__models_results[model_name]["processed_data"]["fcst__dmas_h_q"].loc[start_h:end_h,dma]
-                pred__dma_h_q = pd.concat([pred__dma_h_q, self.__models_results[model_name]["bwdf_forecast"].loc[start_h:end_h,dma]], axis=0)
-                
-                figtraj.add_trace(go.Scatter(x=pred__dma_h_q.index, y=pred__dma_h_q, name=model_name,
-                                             mode='lines', line=dict(color=px.colors.qualitative.Plotly[i])))
-                i += 1
-
-            #end of the for plot the truth. 
-            true__dma_h_q = pd.concat([
-                    self.__train__dmas_h_q.loc[start_h:end_h, dma],
-                    self.__test__dmas_h_q.loc[start_h:end_h, dma]], axis=0)
-            # This starts on the 4th of Jan 2021. So I can simply use the week number
-            figtraj.add_trace(go.Scatter(x=true__dma_h_q.index, y=true__dma_h_q, name='Truth',
-                                            mode='lines', line=dict(color="#000000")))
+            figtraj = figure_dma_q_trajectory(self, dma, pi, model_names, wrange)
 
             return [figpi, 
                     data_loader.load_characteristics().loc[dma,'description'], 

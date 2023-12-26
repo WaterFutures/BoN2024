eae1a0d9aaf977a562417493d761e750b1a8d6e6
jvaquet <jvaquet@techfak.uni-bielefeld.de>
2023-12-26 03:58:35 +0100

    Added normalize preprocessing

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 
 class Preprocessing:
@@ -24,14 +25,26 @@ class Preprocessing:
 
 class Logarithm(Preprocessing):
 
-    def __init__(self):
-        super().__init__()
-
     def transform(self, X):
         return np.log(X)
     
     def inverse_transform(self, X):
         return np.exp(X)
+    
+
+class Normalize(Preprocessing):
+
+    def __init__(self):
+        self.scaler = StandardScaler()
+
+    def fit(self, X):
+        self.scaler.fit(X)
+
+    def transform(self, X):
+        return pd.DataFrame(self.scaler.transform(X), index=X.index, columns=X.columns)
+    
+    def inverse_transform(self, X):
+        return pd.DataFrame(self.scaler.inverse_transform(X), index=X.index, columns=X.columns)
     
 
 

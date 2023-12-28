8d476f1d55b4822b9e69a4108752fce859875ad7
Pansos <pansos@gmail.com>
2023-12-28 08:58:58 +0200

    Added LGBM

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from eval.evaluator import WaterFuturesEvaluator
 from eval.dashboard import run_dashboard
-
 import models
 from models.benchmarks import average_week, previous_week
 from models.autoregressives import autoreg_no_preprocess, autoreg_log, autoreg_log_norm
+from models.LGBM import lgbm_simple
 
 
 wfe = WaterFuturesEvaluator()
@@ -12,6 +12,6 @@ wfe.add_model(previous_week)
 wfe.add_model(average_week)
 wfe.add_model(autoreg_no_preprocess)
 wfe.add_model(autoreg_log)
-wfe.add_model(autoreg_log_norm)
+wfe.add_model(lgbm_simple)
 
 run_dashboard(wfe)

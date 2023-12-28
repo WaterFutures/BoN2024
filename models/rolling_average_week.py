65699449b975f75e0eb8bae2e78d6c2f56b71866
Pansos <pansos@gmail.com>
2023-12-28 08:57:01 +0200

    Instead of weather, added demand_test, weather_test as inputs in forecast function

--- models/rolling_average_week.py
+++ models/rolling_average_week.py
@@ -12,7 +12,7 @@ class RollingAverageWeek(Model):
         demands = demands.to_numpy()
         self.avg_week = np.nanmean(demands.reshape((demands.shape[0] // 168, 168, demands.shape[1]))[-self.window_size:], axis=0)
 
-    def forecast(self, weather):
+    def forecast(self, demand_test, weather_test):
         return np.nan_to_num(self.avg_week, nan=0)
 
 def rolling_average_week(x):

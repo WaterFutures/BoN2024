de2644b59f5d4ce912d7c410a7c506af577468d8
Pansos <pansos@gmail.com>
2024-01-11 16:22:12 +0200

    Added robust LGBM model

--- models/LGBM.py
+++ models/LGBM.py
@@ -41,6 +41,38 @@ class LGBMsimple(Model):
         return pred.T
 
 
+class LGBMrobust(Model):
+    def __init__(self, lgb_params) -> None:
+        super().__init__()
+        self.model = None
+        self.lgb_params = lgb_params
+        self.feats = {}
+        self.model = {}
+
+    def fit(self, demands, weather):
+        self.dmas = DMAS_NAMES
+
+        X_train = demands.merge(weather, right_index=True, left_index=True)
+
+        nan_cols = [c for c in X_train.columns if c.endswith('_nans')]
+        misc_cols = [c for c in X_train.columns if c.startswith('misc_')]
+        rest_lagged_cols = [c for c in X_train.columns if c.endswith('_rest')]
+        lagged_cols = [c for c in X_train.columns if c.endswith('_lagged')]
+        weather_cols = weather.columns.tolist()
+        cat_cols = [c for c in X_train.columns if X_train[c].dtype == 'category']
+
+        for dma in self.dmas:
+            self.feats[dma] = [c for c in lagged_cols + nan_cols if c.startswith(dma)] + weather_cols + cat_cols + misc_cols + [c for c in rest_lagged_cols if c.startswith(dma)]
+            for m in range(0,10):
+                train_set = lgb.Dataset(X_train[self.feats[dma]], label=X_train[dma] + np.random.normal(0, X_train[dma].std() * 0.05, X_train.shape[0]), categorical_feature=cat_cols, free_raw_data=False)
+                self.model[dma+'_'+str(m)] = lgb.train(self.lgb_params, train_set, num_boost_round=1000, categorical_feature=cat_cols)
+
+    def forecast(self, demand_test, weather_test):
+        X_test = demand_test.merge(weather_test, right_index=True, left_index=True)
+        pred = np.array([np.mean(np.array([self.model[dma+'_'+str(m)].predict(X_test[self.feats[dma]]) for m in range(0,10)]), axis=0) for dma in self.dmas])
+        return pred.T
+
+
 lgb_params = {
         'boosting_type': 'gbdt',
         'objective': 'regression',
@@ -55,6 +87,15 @@ lgb_params = {
 lgbm_simple = {
     'name': 'LGBMsimple',
     'model': LGBMsimple(lgb_params = lgb_params),
+    'preprocessing': {
+        'demand': [Logarithm(), LGBM_impute_nan_demand(), LGBM_demand_features()],
+        'weather': [LGBM_impute_nan_weather(), LGBM_weather_features()],
+        'prepare_test_dfs': [LGBM_prepare_test_dfs()]
+    }
+}
+lgbm_robust = {
+    'name': 'LGBMrobust',
+    'model': LGBMrobust(lgb_params = lgb_params),
     'preprocessing': {
         'demand': [Logarithm(), LGBM_impute_nan_demand(), LGBM_demand_features()],
         'weather': [LGBM_impute_nan_weather(), LGBM_weather_features()],

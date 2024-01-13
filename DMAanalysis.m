94d4af2c3d2d7e7d0a26efd68b929b3ae9a2a876
Demetrios G. Eliades <eldemet@gmail.com>
2024-01-14 01:05:29 +0200

    computing demand factors

--- DMAanalysis.m
+++
@@ -1,9 +0,0 @@
-Load data
-addpath(genpath(pwd));
-
-load battledata.mat
-
-TQ = InflowData1.DatetimeCETCESTDDMMYYYYHHmm;
-Q = table2array(InflowData1(:,2:11));
-TW = WeatherData1.DatetimeCETCESTDDMMYYYYHHmm;
-W = table2array(WeatherData1(:,2:5));

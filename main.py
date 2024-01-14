c3cb35863bdbba83843cbb34f9a8efdb630d4cde
Valerie Vaquet <vvaquet@techfak.uni-bielefeld.de>
2024-01-14 18:28:53 +0100

    Add prototype-based model and linear interpolation for simple models
    Add model that predicts based on the mean over the n most similar weeks with respect to the weather conditions.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from eval.dashboard import run_dashboard
 import models
 from models.benchmarks import average_week, previous_week
 from models.autoregressives import autoreg_no_preprocess, autoreg_log, autoreg_log_norm
+from models.prototype_based import prototype_3, prototype_5, prototype_7, prototype_fe, prototype_fe_subset 
 from models.LGBM import lgbm_simple
 
 
@@ -13,5 +14,10 @@ wfe.add_model(average_week)
 wfe.add_model(autoreg_no_preprocess)
 wfe.add_model(autoreg_log)
 wfe.add_model(lgbm_simple)
+wfe.add_model(prototype_3)
+wfe.add_model(prototype_5)
+wfe.add_model(prototype_7)
+wfe.add_model(prototype_fe)
+wfe.add_model(prototype_fe_subset)
 
 run_dashboard(wfe)
--- preprocessing/simple_transforms.py
+++ preprocessing/simple_transforms.py
@@ -26,3 +26,43 @@ class Normalize(Preprocessing):
     
     def inverse_transform(self, X):
         return pd.DataFrame(self.scaler.inverse_transform(X), index=X.index, columns=X.columns)
+    
+
+
+
+
+class LinearInterpolation(Preprocessing):
+
+    def __init__(self, interpolation_range):
+        super().__init__()
+        self.interpolation_range = interpolation_range
+
+    def transform(self, X):
+        
+        header = X.columns
+        index = X.index
+        # interpolating missing values - issue: limit option interpolates limit values for imputation even if many more are missing
+        interpolated = X.interpolate(axis='rows', limit_area='inside', inplace=False).to_numpy()
+        
+        # compute mask to detmine for one dma whether the values should be imputed or not according to self.interpolation_range
+        def compute_mask(input):
+            m = np.ones(len(input), dtype=int)
+
+            for i in range(len(input)-self.interpolation_range):
+                if np.isnan(input[i:i+self.interpolation_range]).sum() == self.interpolation_range:
+                    for j in range(self.interpolation_range):
+                        m[i+j] = 0            
+            return m
+        
+        fct = lambda m, v : v if m else np.nan
+
+        res = np.zeros((interpolated.shape))
+        X = X.to_numpy()
+
+        for dma in range(X.shape[1]):
+            mask = compute_mask(X[:, dma])
+            for i in range(len(res)):
+                res[i, dma] = fct(mask[i], interpolated[i,dma])
+
+        return pd.DataFrame(res, columns=header, index=index)
+
